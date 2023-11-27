8f707b121084760032742e5a48cd16dbf65fd500
morrisaiden <mwaimorris1997@gmail.com>
2023-11-27 12:37:44 -0800

    Linked list cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle
+ * @list: pointer to the head of the linked list
+ * Return: 1 if there is a cycle, 0 otherwise
+ */
+int check_cycle(listint_t *list)
+{
+    listint_t *tortoise, *hare;
+
+    if (list == NULL)
+        return (0);
+
+    tortoise = list;
+    hare = list;
+
+    while (hare != NULL && hare->next != NULL)
+    {
+        tortoise = tortoise->next;
+        hare = hare->next->next;
+
+        if (tortoise == hare)
+            return (1);
+    }
+
+    return (0);
+}
+
