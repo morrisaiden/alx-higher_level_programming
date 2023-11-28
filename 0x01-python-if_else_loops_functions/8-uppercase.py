b7d678aa300e8ac277b50fac02d92b2ade2228d3
morrisaiden <139795199+morrisaiden@users.noreply.github.com>
2023-11-28 22:19:14 +0300

    Update 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
-
-def uppercase(input_str):
-    for char in input_str:
-        if ord('a') <= ord(char) <= ord('z'):
-            print(chr(ord(char) - ord('a') + ord('A')), end="")
-        else:
-            print(char, end="")
+def uppercase(str):
+    
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
     print("")
-
-
-if __name__ == "__main__":
-    uppercase("best")
-    uppercase("Best School 98 Battery street")
 
