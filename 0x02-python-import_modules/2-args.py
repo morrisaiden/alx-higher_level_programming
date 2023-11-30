229fb9e5eb3716b8ecd3c3712b5e256dd1e466f2
morrisaiden <139795199+morrisaiden@users.noreply.github.com>
2023-11-30 22:57:48 +0300

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,10 +2,13 @@
 
 if __name__ == "__main__":
     import sys
-    argc = len(argv) - 1
-    if argc == 0:
+
+    count = len(sys.argv) - 1
+    if count == 0:
         print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
     else:
-        print("{} argument{}:".format(argc, 's' if argc > 1 else ''))
-        for i in range(1, argc + 1):
-            print("{}: {}".format(i, argv[i]))
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
