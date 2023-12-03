a235271d0aaa226beb2f824d39372f4809796cc7
morrisaiden <139795199+morrisaiden@users.noreply.github.com>
2023-12-03 21:04:18 +0300

    13-is-palindrome.c

--- 0x03-python-data_structures/13-is-palindrome.c
+++ 0x03-python-data_structures/13-is-palindrome.c
@@ -1,56 +1,63 @@
 #include "lists.h"
 
-listint_t *reverse_list(listint_t **head)
+/**
+ * reverse_listint - reverses a linked list
+ * @head: pointer to the first node in the list
+ */
+void reverse_listint(listint_t **head)
 {
-    listint_t *prev = NULL, *current = *head, *next;
+	listint_t *prev = NULL;
+	listint_t *current = *head;
+	listint_t *next = NULL;
 
-    while (current != NULL)
-    {
-        next = current->next;
-        current->next = prev;
-        prev = current;
-        current = next;
-    }
+	while (current)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
 
-    *head = prev;
-    return *head;
+	*head = prev;
 }
-
 int is_palindrome(listint_t **head)
 {
-    if (*head == NULL || (*head)->next == NULL)
-        return 1;
+	listint_t *slow = *head, *fast = *head, *temp = *head, *dup = NULL;
 
-    listint_t *original_head = *head;
-    listint_t *slow = *head, *fast = *head, *prev_slow = *head;
-    listint_t *second_half, *reversed_second_half;
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
 
-    while (fast != NULL && fast->next != NULL)
-    {
-        fast = fast->next->next;
-        prev_slow = slow;
-        slow = slow->next;
-    }
+	while (1)
+	{
+		fast = fast->next->next;
+		if (!fast)
+		{
+			dup = slow->next;
+			break;
+		}
+		if (!fast->next)
+		{
+			dup = slow->next->next;
+			break;
+		}
+		slow = slow->next;
+	}
 
-    if (fast != NULL)
-        slow = slow->next;
+	reverse_listint(&dup);
 
-    second_half = slow;
-    reversed_second_half = reverse_list(&second_half);
+	while (dup && temp)
+	{
+		if (temp->n == dup->n)
+		{
+			dup = dup->next;
+			temp = temp->next;
+		}
+		else
+			return (0);
+	}
 
-    *head = original_head; // Restore the head pointer to the original position
+	if (!dup)
+		return (1);
 
-    while (reversed_second_half != NULL)
-    {
-        if (original_head->n != reversed_second_half->n)
-            return 0;
-
-        original_head = original_head->next;
-        reversed_second_half = reversed_second_half->next;
-    }
-
-    reverse_list(&second_half);
-    prev_slow->next = second_half;
-
-    return 1;
+	return (0);
 }
