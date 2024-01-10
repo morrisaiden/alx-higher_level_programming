c2e872fcbf71fd00f591080743bc72ef76ddc191
morrisaiden <mwaimorris1997@gmail.com>
2024-01-10 13:46:05 -0800

    Square #1

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+module 10-square.
+inherits from Rectangle (9-rectangle.py):
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+"""
+Square class
+"""
+
+
+class Square(Rectangle):
+    """
+    Square Class
+    """
+    def __init__(self, size):
+        """
+        instantiation with size
+        """
+        self.__size = size
+        super().__init__(self.__size, self.__size)
