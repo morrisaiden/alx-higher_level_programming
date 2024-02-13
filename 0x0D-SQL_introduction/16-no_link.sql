cf821f6e20d19787ef3a7b65c561bde876431ab4
morrisaiden <139795199+morrisaiden@users.noreply.github.com>
2024-02-13 16:09:58 +0300

    Say may name

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -3,9 +3,4 @@
 -- Results should display the score and the name (in this order)
 -- Records should be listed by descending score
 
-INSERT INTO second_table (score, name)
-VALUES(18, 'Aria'),
-(12, 'Aria');
-DELETE FROM second_table WHERE name="George";
-
 SELECT score, name FROM second_table WHERE name IS NOT NULL AND name !='' ORDER BY score DESC;
