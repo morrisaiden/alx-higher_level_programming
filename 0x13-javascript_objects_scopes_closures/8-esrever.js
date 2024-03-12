eeea28564c0ea6197a2e0d932406053a12796a0c
morrisaiden <mwaimorris@gmail.com>
2024-03-12 11:08:00 -0700

    Esrever

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+  const reversed = [];
+
+  for (let j = list.length - 1; j >= 0; j--) {
+    reversed.push(list[i]);
+  }
+
+  return reversed;
+};
