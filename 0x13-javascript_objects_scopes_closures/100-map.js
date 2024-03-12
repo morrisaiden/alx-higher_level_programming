48685bb84a6f22c80ae99ff112946835175f4347
morrisaiden <139795199+morrisaiden@users.noreply.github.com>
2024-03-12 21:46:02 +0300

    Delete

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-const list = require('./100-data').list;
-
-const mapped = list.map(function (value, index) { return value * index; });
-
-console.log(list);
-
-console.log(mapped);
