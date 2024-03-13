18bbb9de74deaf1cb2c42672790eaa870201d864
morrisaiden <139795199+morrisaiden@users.noreply.github.com>
2024-03-13 19:13:41 +0300

    Add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,15 +1,11 @@
 #!/usr/bin/node
 
-function add(a, b) {
-  return a + b;
+const first = parseInt(process.argv[2]);
+const second = parseInt(process.argv[3]);
+function add (a, b) {
+    if (isNaN(a) || isNaN(b)) {
+	return NaN;
+    }
+    return a + b;
 }
-
-const arg1 = parseInt(process.argv[2]);
-const arg2 = parseInt(process.argv[3]);
-
-if (!isNaN(arg1) && !isNaN(arg2)) {
-  console.log(add(arg1, arg2));
-} else {
-  console.log("Invalid input. Please provide two integers.");
-}
-
+console.log(add(first, second));
